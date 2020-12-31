b22568db25aba09504d0a7a63050f078905df159
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2020-12-31 12:07:49 +0100

    Add bullets (without collision)

---
+++ src/Bullet.java
@@ -0,0 +1,26 @@
+import java.awt.Graphics;
+import java.awt.Color;
+
+public class Bullet {
+    static int count = 0;
+    int pos_x, pos_y;
+    public static int width = 3;
+    public static int height = 12;
+    static int v = 10;
+
+    public Bullet(Player player) {
+        this.pos_x = player.pos_x;
+        this.pos_y = player.pos_y;
+        count++;
+    }
+
+    void move() {
+        pos_y -= v;
+    }
+
+    void render(Graphics graphics) {
+        graphics.setColor(Color.WHITE);
+        graphics.fillRect(pos_x - width/2, pos_y, width, height);
+    }
+}
+
--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.awt.Graphics;
 import java.awt.Color;
 import java.awt.event.*;
@@ -8,6 +9,7 @@ public class Player implements KeyListener {
     int v_l = 0, v_r = 0;
     static int v_increment = 10;
     static int width = 30, height = 10;
+    ArrayList<Bullet> bullets = new ArrayList<Bullet>();
 
     public Player(int pos_x, int pos_y) {
         this.pos_x = pos_x;
@@ -21,17 +23,20 @@ public class Player implements KeyListener {
         if(pos_x > 0.10*GameFrame.WIDTH) {
             pos_x += v_l;
         }
+        for(Bullet bullet: bullets) {
+            bullet.move();
+        }
     }
 
     void render(Graphics graphics) {
         graphics.setColor(Color.WHITE);
         graphics.fillRect(pos_x - width/2, pos_y, width, height);
+        for(Bullet bullet: bullets) {
+            bullet.render(graphics);
+        }
     }
 
     public void keyTyped(KeyEvent e) {
-        if(e.getKeyChar() == ' ') {
-            System.out.println("piu piu");
-        }
     }
 
     public void keyReleased(KeyEvent e) {
@@ -51,6 +56,8 @@ public class Player implements KeyListener {
         } else if(!vb_r && (e.getKeyCode() == KeyEvent.VK_D)) {
             vb_r = true;
             v_r += v_increment;
+        } else if(e.getKeyCode() == KeyEvent.VK_SPACE) {
+            bullets.add(new Bullet(this));
         }
     }
 }
