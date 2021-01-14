1d7e5e1158d6f942fc9f33ced83ce4ed324404bb
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-01-14 11:26:15 +0100

    Make Enemy extend Entity

--- src/game/Enemy.java
+++ src/game/Enemy.java
@@ -2,15 +2,13 @@ package game;
 import java.awt.Graphics;
 import java.awt.Color;
 
-public class Enemy {
+public class Enemy extends Entity {
 	static int count = 0;
-	int pos_x, pos_y;
 	public static int width = 30;
 	public static int height = 20;
 
 	public Enemy(int pos_x, int pos_y) {
-	    this.pos_x = pos_x;
-	    this.pos_y = pos_y;
+	    super(pos_x, pos_y);
 	    count++;
 	}
 
