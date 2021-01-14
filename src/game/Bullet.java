7461d7ef7ec4463546d30ac9bd32059781ec6e2a
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-01-14 12:14:47 +0100

    Make bullet speed a parameter in move()

--- src/game/Bullet.java
+++ src/game/Bullet.java
@@ -7,15 +7,14 @@ public class Bullet extends Entity {
     static int count = 0;
     public static int width = 3;
     public static int height = 12;
-    static int v = 12;
 
     public Bullet(Player player) {
     	super(player.pos_x, player.pos_y);
         count++;
     }
 
-    void move() {
-        pos_y -= v;
+    void move(int v) {
+        pos_y += v;
     }
 
     void render(Graphics graphics) {
