ad77416d4993b56d89be6060f57dabf2a82cc04b
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-01-18 00:50:52 +0100

    Add method to check if player bullet is offscreen

--- src/game/Bullet.java
+++ src/game/Bullet.java
@@ -21,4 +21,11 @@ public class Bullet extends Entity {
         graphics.setColor(Color.WHITE);
         graphics.fillRect(x - width/2, y, width, height);
     }
+    
+    public boolean outOfBounds() {
+    	if(y <= 0) {
+    		return true;
+    	}
+    	return false;
+    }
 }
