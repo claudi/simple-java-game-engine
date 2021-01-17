18d498d250988f88e8d30efbcc6c1115dbb32e2b
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-01-18 00:49:43 +0100

    Add start of collision function for Entity class

--- src/game/Entity.java
+++ src/game/Entity.java
@@ -8,4 +8,21 @@ public class Entity {
 	    this.x = x;
 	    this.y = y;
 	}
+	
+	public boolean collision(Entity entity) {
+		boolean rightGTleft = x + width >= entity.x - Entity.width;
+		boolean leftLTright = x - width <= entity.x + Entity.width;
+		boolean topLTbottom = y <= entity.y + Entity.height;
+		boolean bottomGTtop = y + height >= entity.y;
+		System.out.print(String.valueOf(rightGTleft));
+		System.out.print(String.valueOf(leftLTright));
+		System.out.print(String.valueOf(topLTbottom));
+		System.out.print(String.valueOf(bottomGTtop));
+		System.out.print("\n");
+		if(rightGTleft && bottomGTtop && leftLTright && topLTbottom) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
