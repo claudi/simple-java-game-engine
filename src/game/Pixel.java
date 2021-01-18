fb5508718f04a32594b3f4b432cd8c38cd8daeee
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-01-18 02:02:29 +0100

    Add Pixel class

---
+++ src/game/Pixel.java
@@ -0,0 +1,26 @@
+package game;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Pixel extends Entity {
+	private final int width = 5, height = 5;
+	private Color color;
+	
+	public Pixel(int pos_x, int pos_y, Color color) {
+		this.pos_x = pos_x;
+		this.pos_y = pos_y;
+		this.color = color;
+	}
+	
+	void render(Graphics graphics) {
+		graphics.setColor(color);
+		graphics.fillRect(pos_x, pos_y, width, height);
+	}
+
+	void move(int dx, int dy) {
+		pos_x += dx;
+		pos_y += dy;
+	}
+	
+}
