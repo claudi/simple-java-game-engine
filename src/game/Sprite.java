a2fb07bb15a1e765e8d79843d643e50c2130543b
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-01-18 02:07:36 +0100

    Add Sprite class

---
+++ src/game/Sprite.java
@@ -0,0 +1,22 @@
+package game;
+
+import java.awt.Graphics;
+import java.util.ArrayList;
+
+public class Sprite extends Entity {
+	ArrayList<Pixel> pixels = new ArrayList<Pixel>();
+	
+	void render(Graphics graphics) {
+		for(Pixel pixel: pixels) {
+			pixel.render(graphics);
+		}
+	}
+
+	void move(int dx, int dy) {
+		for(Pixel pixel: pixels) {
+			pixel.move(dx, dy);
+		}
+		pos_x += dx;
+		pos_y += dy;
+	}
+}
