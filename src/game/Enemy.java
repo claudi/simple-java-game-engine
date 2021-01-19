1c40904c0aed5247480ccc2d1d805f770793193d
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-01-19 23:13:13 +0100

    Add typical space invider sprite to Space invader

--- src/game/Enemy.java
+++ src/game/Enemy.java
@@ -5,15 +5,18 @@ import java.awt.Color;
 public class Enemy extends Sprite {
 	static int count = 0;
 	public static int width = 30;
-	public static int height = 20;
+	public static int height = 25;
 	static final Color color = Color.WHITE;
 
 	static boolean pixel_array[][] = {
-			{false, true, true, false},
-			{true, false, false, true},
-			{true, false, false, true},
-			{true, true, true, true},
-			{true, false, false, true}
+			{false, false,  true, false, false, false, false,  true, false, false},
+			{false, false, false,  true, false, false,  true, false, false, false},
+			{false, false,  true,  true,  true,  true,  true,  true, false, false},
+			{false,  true,  true, false,  true,  true, false,  true,  true, false},
+			{ true,  true,  true,  true,  true,  true,  true,  true,  true,  true},
+			{ true, false,  true,  true,  true,  true,  true,  true, false,  true},
+			{false, false, false,  true, false, false,  true, false, false, false},
+			{false, false,  true, false, false, false, false,  true, false, false}
 	};
 
 	public Enemy(int pos_x, int pos_y) {
