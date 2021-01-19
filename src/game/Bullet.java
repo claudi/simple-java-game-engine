4f07d53a17468168d8207a4eb33c4bab8670e77d
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-01-19 23:02:19 +0100

    Add basic Bullet sprite

--- src/game/Bullet.java
+++ src/game/Bullet.java
@@ -6,14 +6,20 @@ public class Bullet extends Sprite {
 	static int count = 0;
     public static int width = 3;
     public static int height = 12;
-    private final Color color = Color.WHITE;
+    static final Color color = Color.WHITE;
+
+	static boolean pixel_array[][] = {
+			{true},
+			{true},
+			{true},
+			{true},
+			{true}
+	};
 
     public Bullet(int pos_x, int pos_y) {
+    	super(pixel_array, pos_x, pos_y, color);
     	this.pos_x = pos_x;
     	this.pos_y = pos_y;
-    	for(int iter = 0; iter < 4; iter++) {
-    		pixels.add(new Pixel(pos_x, pos_y + iter*Pixel.height, color));
-    	}
     	count++;
     }
 
