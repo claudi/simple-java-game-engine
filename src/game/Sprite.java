506a6bd3cd3a966c0992aaeb0eed690a6634e9bd
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-01-21 21:48:52 +0100

    Add width and height variables into Sprite

--- src/game/Sprite.java
+++ src/game/Sprite.java
@@ -5,19 +5,23 @@ import java.awt.Graphics;
 import java.util.ArrayList;
 
 public abstract class Sprite extends Entity {
-	int height, width;
+	int height, width;					// Total sizes of the sprite
+	int pixels_height, pixels_width;	// Dimensions of pixel array
 	ArrayList<Pixel> pixels = new ArrayList<Pixel>();
-	
+
 	public Sprite(Color[][] pixel_array, int pos_x, int pos_y) {
-		this.height = pixel_array.length;
-		this.width = pixel_array[0].length;
-		for(int i = 0; i < width; i++) {
-			for(int j = 0; j < height; j++) {
+		this.pixels_height = pixel_array.length;
+		this.pixels_width = pixel_array[0].length;
+		for(int i = 0; i < pixels_width; i++) {
+			for(int j = 0; j < pixels_height; j++) {
 				if(pixel_array[j][i] != null) {
-					pixels.add(new Pixel(pos_x + (i - width/2)*Pixel.width, pos_y + j*Pixel.height, pixel_array[j][i]));
+					pixels.add(new Pixel(pos_x + (i - pixels_width/2)*Pixel.width, pos_y + j*Pixel.height, pixel_array[j][i]));
 				}
 			}
 		}
+
+		this.height = Pixel.height * pixels_height;
+		this.width = Pixel.width * pixels_width;
 	}
 
 	void render(Graphics graphics) {
