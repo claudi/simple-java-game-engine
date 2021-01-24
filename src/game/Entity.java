f12636431cf09222132e257d1ce377e011398c5b
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-01-24 12:20:40 +0100

    Fix typo

--- src/game/Entity.java
+++ src/game/Entity.java
@@ -6,5 +6,5 @@ abstract public class Entity {
 	int pos_x, pos_y; // Central position of Entity 
 	abstract void render(Graphics graphics);
 	abstract void move(int dx, int dy);
-	abstract boolean collision(Entity entiry);
+	abstract boolean collision(Entity entity);
 }
