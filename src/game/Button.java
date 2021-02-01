e5e6f3be41609fac3e8892abf6c4e592d5ca8037
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-02-01 21:42:25 +0100

    Made Button variables into private
    See issue #25

--- src/game/Button.java
+++ src/game/Button.java
@@ -4,9 +4,9 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class Button {
-	int pos_x, pos_y;
-	int width, height;
-	String label;
+	private int pos_x, pos_y;
+	private int width, height;
+	private String label;
 
 	public Button(int pos_x, int pos_y, String label, Graphics graphics) {
 		this.label = label.toUpperCase();
@@ -14,6 +14,22 @@ public class Button {
 		this.width = graphics.getFontMetrics(GameFrame.menu_font).stringWidth(label);
 		this.pos_x = GameFrame.WIDTH/2 - width/2;
 		this.pos_y = pos_y;
+	}
+
+	int getPosX() {
+		return pos_x;
+	}
+
+	int getPosY() {
+		return pos_y;
+	}
+
+	int getHeight() {
+		return height;
+	}
+
+	int getWidth() {
+		return width;
 	}
 
 	void render(Graphics graphics) {
