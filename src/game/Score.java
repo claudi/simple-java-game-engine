9d36759bba76e66463d22840b7e882e749b2d9db
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-02-01 16:34:09 +0100

    Move rendering from Graphics2D to Graphics

--- src/game/Score.java
+++ src/game/Score.java
@@ -2,7 +2,6 @@ package game;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 
 public class Score {
 	int enemies_hit;
@@ -32,9 +31,8 @@ public class Score {
 	}
 
 	public void render(Graphics graphics) {
-		Graphics2D g2 = (Graphics2D) graphics;
-		g2.setColor(Color.WHITE);
-		g2.setFont(GameFrame.font);
-		g2.drawString("SCORE: " + String.valueOf(enemies_hit + bullets_hit + bullets_intercepted), 10, 40);
+		graphics.setColor(Color.WHITE);
+		graphics.setFont(GameFrame.font);
+		graphics.drawString("SCORE: " + String.valueOf(enemies_hit + bullets_hit + bullets_intercepted), 10, 40);
 	}
 }
