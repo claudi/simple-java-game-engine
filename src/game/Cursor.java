58a3b9077450efe1e7a2c09091a4687209852526
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-02-01 21:43:39 +0100

    Implement Cursor class

---
+++ src/game/Cursor.java
@@ -0,0 +1,35 @@
+package game;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Cursor {
+	private int pos_x, pos_y;
+	private int width, height;
+	private String label;
+
+	public Cursor(int pos_x, int pos_y, Graphics graphics) {
+		this.label = ">";
+		this.height = graphics.getFontMetrics(GameFrame.menu_font).getAscent();
+		this.width = graphics.getFontMetrics(GameFrame.menu_font).stringWidth(label);
+		this.pos_x = GameFrame.WIDTH/2 - width/2;
+		this.pos_y = pos_y;
+	}
+
+	
+	void setPos(Button button) {
+		setPos(button.getPosX(), button.getPosY());
+	}
+
+	void setPos(int pos_x, int pos_y) {
+		this.pos_x = pos_x - 2*width;
+		this.pos_y = pos_y - height/4;
+	}
+	
+	void render(Graphics graphics) {
+		graphics.setColor(Color.WHITE);
+		graphics.setFont(GameFrame.menu_font);
+		graphics.drawString(label, pos_x, pos_y + height);
+	}
+
+}
