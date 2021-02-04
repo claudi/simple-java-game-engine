5b3152e2f5c96a237991fe993f700b67eeeb26a9
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-02-04 17:08:59 +0100

    Format Enemies.java

--- src/game/Enemies.java
+++ src/game/Enemies.java
@@ -4,55 +4,54 @@ import java.awt.Graphics;
 
 public class Enemies extends Collection<Enemy> {
 	int n, m;
-	int pos_x = GameFrame.WIDTH/2, pos_y;
+	int pos_x = GameFrame.WIDTH / 2, pos_y;
 	public int width, height;
 	int v = 3;
 	private int bullet_speed = 12;
 	Bullets bullets = new Bullets(bullet_speed);
 
 	Enemies(int n, int m) {
-	    this.n = n;
-	    this.m = m;
+		this.n = n;
+		this.m = m;
 
-	    int enemy_width = Pixel.height * Enemy.pixel_array[0].length;
-	    int enemy_height = Pixel.width * Enemy.pixel_array.length;
+		int enemy_width = Pixel.height * Enemy.pixel_array[0].length;
+		int enemy_height = Pixel.width * Enemy.pixel_array.length;
 
-	    int pad_x = Math.round((float) 2*enemy_width);
-	    int pad_y = Math.round((float) 1.5*enemy_height);
+		int pad_x = Math.round((float) 2 * enemy_width);
+		int pad_y = Math.round((float) 1.5 * enemy_height);
 
-	    this.pos_y = pad_y;
+		this.pos_y = pad_y;
 
-	    width = pad_x*(n-1) + enemy_width;
-	    height = pad_y*(n-1) + enemy_height;
+		width = pad_x * (n - 1) + enemy_width;
+		height = pad_y * (n - 1) + enemy_height;
 
-	    for(int i = 0; i < n; i++) {
-	        for(int j = 0; j < m; j++) {
-	            elements.add(new Enemy(pos_x + enemy_width/2 + pad_x*i - width/2,
-	                                  pos_y + pad_y*j));
-	        }
-	    }
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				elements.add(new Enemy(pos_x + enemy_width / 2 + pad_x * i - width / 2, pos_y + pad_y * j));
+			}
+		}
 	}
 
 	void move() {
 		bullets.move();
 
-		for(Enemy enemy: elements) {
-			if(enemy.pos_x - 4*enemy.width < 0 || enemy.pos_x + 4*enemy.width > GameFrame.WIDTH) {
+		for (Enemy enemy : elements) {
+			if (enemy.pos_x - 4 * enemy.width < 0 || enemy.pos_x + 4 * enemy.width > GameFrame.WIDTH) {
 				v = -v;
 				break;
 			}
 		}
 
-	    for(Enemy enemy: elements) {
-	        enemy.move(v, 0);
-	    }
-	    pos_x += v;
+		for (Enemy enemy : elements) {
+			enemy.move(v, 0);
+		}
+		pos_x += v;
 	}
 
 	private boolean isLastInColumn(Enemy enemy) {
-		for(Enemy other_enemy: elements) {
-			if(other_enemy.pos_x == enemy.pos_x) {
-				if(other_enemy.pos_y > enemy.pos_y) {
+		for (Enemy other_enemy : elements) {
+			if (other_enemy.pos_x == enemy.pos_x) {
+				if (other_enemy.pos_y > enemy.pos_y) {
 					return false;
 				}
 			}
@@ -61,20 +60,20 @@ public class Enemies extends Collection<Enemy> {
 	}
 
 	void endFrame() {
-		for(Enemy enemy: elements) {
-      if(isLastInColumn(enemy) && GameFrame.random.nextInt(50) == 0) {
+		for (Enemy enemy : elements) {
+			if (isLastInColumn(enemy) && GameFrame.random.nextInt(50) == 0) {
 				bullets.add(enemy.shoot());
 			}
 		}
-		for(Enemy enemy: elements) {
+		for (Enemy enemy : elements) {
 			enemy.endFrame();
 		}
 	}
 
 	void render(Graphics graphics) {
 		bullets.render(graphics);
-	    for(Enemy enemy: elements) {
-	        enemy.render(graphics);
-	    }
+		for (Enemy enemy : elements) {
+			enemy.render(graphics);
+		}
 	}
 }
