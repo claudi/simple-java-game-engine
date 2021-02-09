729433944e05f64fe2e4a7024d74cb1d4d0cf680
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-02-09 19:08:37 +0100

    Add automatic total score calculation

--- src/game/Score.java
+++ src/game/Score.java
@@ -8,6 +8,7 @@ public class Score {
 	int bullets_hit;
 	int bullets_missed;
 	int bullets_intercepted;
+	int deducted;
 	int level;
 
 	public Score() {
@@ -15,7 +16,20 @@ public class Score {
 		bullets_hit = 0;
 		bullets_missed = 0;
 		bullets_intercepted = 0;
+		deducted = 0;
 		level = 1;
+	}
+	
+	public int getScore() {
+		return enemies_hit + bullets_hit + bullets_intercepted - deducted;
+	}
+	
+	public boolean deductScore(int val) {
+		if(getScore() < val) {
+			return false;
+		}
+		this.deducted += val;
+		return true;
 	}
 
 	public void hitEnemy() {
@@ -39,7 +53,7 @@ public class Score {
 	public void render(Graphics graphics) {
 		graphics.setColor(Color.WHITE);
 		graphics.setFont(GameFrame.font);
-		graphics.drawString("SCORE: " + String.valueOf(enemies_hit + bullets_hit + bullets_intercepted), 10, 40);
+		graphics.drawString("SCORE: " + String.valueOf(getScore()), 10, 40);
 		graphics.drawString("LEVEL: " + String.valueOf(level), 10, 70);
 	}
 }
