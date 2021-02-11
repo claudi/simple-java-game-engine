e625be761a590dfd9b0d828d903ae31c80c8f207
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-02-11 17:32:18 +0100

    Remove public modificator from class

--- src/game/Entity.java
+++ src/game/Entity.java
@@ -2,7 +2,7 @@ package game;
 
 import java.awt.Color;
 
-abstract public class Entity extends Sprite {
+abstract class Entity extends Sprite {
 	public Entity(Color[][] pixel_array, int pos_x, int pos_y) {
 		super(pixel_array, pos_x, pos_y);
 		this.pos_x = pos_x;
