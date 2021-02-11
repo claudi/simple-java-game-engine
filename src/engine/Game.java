cda0e312adf91f7fc0cd81c4b9b02515bc1e9e5f
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-02-11 21:00:15 +0100

    Add main game loop

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -1,12 +1,14 @@
 package engine;
 
 import java.awt.Color;
+import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Iterator;
 
 public class Game {
 	GameFrame frame;
@@ -39,6 +41,116 @@ public class Game {
 	
 	public int getHeight() {
 		return frame.getHeight();
+	}
+	
+	public void mainLoop() {
+		frame.addKeyListener(player);
+		
+		while (!gameOver()) {
+			if (frame.isActive()) {
+				makeMoves();
+				detectCollisions();
+				render();
+				endFrame();
+			}
+			delay();
+		}
+
+		if (player.isAlive()) { // Player win
+			System.out.println("Player win");
+		} else if (enemies.size() > 0) { // Enemies win
+			quitGame();
+		} else { // Tie
+			quitGame();
+		}
+	}
+	
+	private void makeMoves() {
+		player.move();
+		enemies.move();
+	}
+
+	void detectCollisions() {
+		Iterator<Bullet> bullets_iterator;
+		Iterator<Bullet> enemies_bullets_iterator;
+		Iterator<Enemy> enemies_iterator;
+
+		bullets_iterator = player.bullets.iterator();
+		while (bullets_iterator.hasNext()) {
+			Bullet player_bullet = bullets_iterator.next();
+			enemies_bullets_iterator = enemies.bullets.iterator();
+			while (enemies_bullets_iterator.hasNext()) {
+				Bullet enemy_bullet = enemies_bullets_iterator.next();
+				if (player_bullet.collision(enemy_bullet)) {
+					bullets_iterator.remove();
+					enemies_bullets_iterator.remove();
+					break;
+				}
+			}
+		}
+
+		enemies_bullets_iterator = enemies.bullets.iterator();
+		while (enemies_bullets_iterator.hasNext()) {
+			Bullet bullet = enemies_bullets_iterator.next();
+			if (bullet.outOfBounds()) {
+				enemies_bullets_iterator.remove();
+			} else {
+				if (bullet.collision(player)) {
+					enemies_bullets_iterator.remove();
+					player.hit();
+				}
+			}
+		}
+
+		bullets_iterator = player.bullets.iterator();
+		while (bullets_iterator.hasNext()) {
+			Bullet bullet = bullets_iterator.next();
+
+			if (bullet.outOfBounds()) {
+				bullets_iterator.remove();
+			} else {
+				enemies_iterator = enemies.iterator();
+				while (enemies_iterator.hasNext()) {
+					Enemy enemy = enemies_iterator.next();
+
+					if (bullet.collision(enemy)) {
+						enemy.hit();
+						bullets_iterator.remove();
+						enemies_iterator.remove();
+					}
+				}
+			}
+		}
+	}
+
+	void render() {
+		frame.render();
+		player.render(frame.graphics);
+		enemies.render(frame.graphics);
+		frame.repaint();
+	}
+
+	void delay() {
+		try {
+			Thread.sleep(50);
+		} catch (InterruptedException e) {
+		}
+	}
+
+	void endFrame() {
+		player.endFrame();
+		enemies.endFrame();
+	}
+
+	boolean gameOver() {
+		if ((enemies.bullets.size() == 0 && enemies.size() == 0) || player.isDead()) {
+			return true;
+		}
+		return false;
+	}
+
+	public void quitGame() {
+		frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
 	}
 	
 	public static Color[][] sprites(String name) {
