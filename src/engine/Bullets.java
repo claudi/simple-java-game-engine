af838372e549f715b2b9954319eec0554afb95d0
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-02-11 17:49:18 +0100

    Add abstract Bullets class in engine

---
+++ src/engine/Bullets.java
@@ -0,0 +1,13 @@
+package engine;
+
+import java.awt.Graphics;
+
+abstract public class Bullets extends Collection<Bullet> {
+	public abstract void move(int dx, int dy);
+	
+	public void render(Graphics graphics) {
+		for(Bullet bullet: elements) {
+			bullet.render(graphics);
+		}
+	}
+}
