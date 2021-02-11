e04f4f1c5952c5dcb4fe97bfacff822ad9fbb555
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-02-11 14:20:10 +0100

    Add start of Bullet class in engine

---
+++ src/engine/Bullet.java
@@ -0,0 +1,19 @@
+package engine;
+
+import java.awt.Color;
+
+abstract public class Bullet extends Entity {
+	static Color pixel_array[][] = Game.sprites("bullet");
+
+	public Bullet(int pos_x, int pos_y) {
+		super(pixel_array, pos_x, pos_y);
+	}
+
+	void move(int v) {
+		super.move(0, v);
+	}
+
+	abstract public void endFrame();
+
+	abstract public boolean outOfBounds();
+}
