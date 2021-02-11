d84929cdf0719fcf84fcb86e50c9c2568b68fa2a
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-02-11 13:42:05 +0100

    Add static method to initialize sprites to Game
    Consider moving it to the Sprite class?
    
    See issue #71

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -1,6 +1,12 @@
 package engine;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
 
 public class Game {
 	GameFrame frame;
@@ -28,5 +34,51 @@ public class Game {
 	
 	public int getHeight() {
 		return frame.getHeight();
+	}
+	
+	public static Color[][] sprites(String name) {
+		int width, height, colours;
+		String file = "./res/" + name + ".xpm";
+		Color sprite[][] = null;
+
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader(file));
+			String line = reader.readLine();
+
+			String dims[] = line.split(" ");
+			height = Integer.parseInt(dims[0]);
+			width = Integer.parseInt(dims[1]);
+			colours = Integer.parseInt(dims[2]);
+
+			sprite = new Color[height][width];
+
+			HashMap<Character, Color> colour = new HashMap<Character, Color>();
+			colour.put(' ', null);
+			for (int i = 0; i < colours; i++) {
+				String[] colour_line = reader.readLine().split(" ");
+				colour.put(colour_line[0].charAt(0), new Color(Integer.parseInt(colour_line[1]),
+						Integer.parseInt(colour_line[2]), Integer.parseInt(colour_line[3])));
+			}
+			for (int i = 0; i < height; i++) {
+				for (int j = 0; j < width; j++) {
+					int c = reader.read();
+					if (c == '\n') {
+						c = reader.read();
+					}
+					sprite[i][j] = colour.get((char) c);
+				}
+			}
+
+			reader.close();
+		} catch (FileNotFoundException e) {
+			System.err.println("ERROR: file \"" + file + "\" not found");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.err.println("ERROR: IOException");
+			e.printStackTrace();
+		}
+
+		return sprite;
 	}
 }
