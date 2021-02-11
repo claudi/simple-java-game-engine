911b2a151bcec7fa82a491c6c43e3ea4685f82a0
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-02-11 17:38:57 +0100

    Remove pixel_array from Bullet in favour of Sprite

--- src/engine/Bullet.java
+++ src/engine/Bullet.java
@@ -1,11 +1,7 @@
 package engine;
 
-import java.awt.Color;
-
 abstract public class Bullet extends Entity {
-	static Color pixel_array[][] = Game.sprites("bullet");
-
 	public Bullet(int pos_x, int pos_y) {
-		super(pixel_array, pos_x, pos_y);
+		super(pos_x, pos_y);
 	}
 }
