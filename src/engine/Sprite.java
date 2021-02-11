93f1a255a202f4d5783a275f4f42d056a6895b9f
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-02-11 13:31:00 +0100

    Add Sprite class to engine

---
+++ src/engine/Sprite.java
@@ -0,0 +1,46 @@
+package engine;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Sprite extends Collection<Pixel> {
+	private int height, width; // Total sizes of the sprite
+	private int pixels_height, pixels_width; // Dimensions of pixel array
+
+	public Sprite(Color[][] pixel_array, int pos_x, int pos_y) {		
+		this.pixels_height = pixel_array.length;
+		this.pixels_width = pixel_array[0].length;
+		
+		for (int i = 0; i < pixels_width; i++) {
+			for (int j = 0; j < pixels_height; j++) {
+				if (pixel_array[j][i] != null) {
+					elements.add(new Pixel(pos_x + (i - pixels_width / 2) * Pixel.width, pos_y + j * Pixel.height,
+							pixel_array[j][i]));
+				}
+			}
+		}
+
+		this.height = Pixel.height * pixels_height;
+		this.width = Pixel.width * pixels_width;
+	}
+	
+	public void move(int dx, int dy) {
+		for(Pixel pixel: elements) {
+			pixel.move(dx, dy);
+		}
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+	
+	void render(Graphics graphics) {
+		for(Pixel pixel: elements) {
+			pixel.render(graphics);
+		}
+	}
+}
