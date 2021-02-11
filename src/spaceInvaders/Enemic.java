52b4a4a826a75d8d131f8ed5d94cfc2154c8385e
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-02-11 19:20:36 +0100

    Add sprite for Enemic

--- src/spaceInvaders/Enemic.java
+++ src/spaceInvaders/Enemic.java
@@ -1,9 +1,14 @@
 package spaceInvaders;
 
+import java.awt.Color;
+
 import engine.Bullet;
 import engine.Enemy;
+import engine.Game;
 
 public class Enemic extends Enemy {
+	static Color[][] pixel_array = Game.sprites("alien");
+	
 	public Enemic(int pos_x, int pos_y) {
 		super(pos_x, pos_y);
 	}
