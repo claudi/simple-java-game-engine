c7d293cd959edc086bcefeb2bfda706d6db38d03
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-02-11 13:07:22 +0100

    Refactor Player side movement to hide KeyListener

--- src/engine/Player.java
+++ src/engine/Player.java
@@ -4,19 +4,36 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 abstract public class Player extends Entity implements KeyListener {
-	abstract void moveLeftCommand();
-	abstract void moveRightCommand();
-	abstract void shootCommand();
+	protected abstract void moveLeftCommand(boolean status);
+	protected abstract void moveRightCommand(boolean status);
+	protected abstract void shootCommand();
+
+	public void keyTyped(KeyEvent e) {}
+	
+	public void keyReleased(KeyEvent e) {
+		switch(e.getKeyCode()) {
+		case KeyEvent.VK_A:
+		case KeyEvent.VK_LEFT:
+			moveLeftCommand(false);
+			break;
+		case KeyEvent.VK_W:
+		case KeyEvent.VK_RIGHT:
+			moveRightCommand(false);
+			break;
+		default:
+				break;
+		}
+	}
 
 	public void keyPressed(KeyEvent e) {
 		switch(e.getKeyCode()) {
 		case KeyEvent.VK_A:
 		case KeyEvent.VK_LEFT:
-			moveLeftCommand();
+			moveLeftCommand(true);
 			break;
 		case KeyEvent.VK_W:
 		case KeyEvent.VK_RIGHT:
-			moveRightCommand();
+			moveRightCommand(true);
 			break;
 		case KeyEvent.VK_SPACE:
 			shootCommand();
