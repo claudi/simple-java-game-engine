241522f3e9b7633540fb5c01b6ccaad929c24ec3
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-02-11 13:23:10 +0100

    Change wrong access modifier to Collection<E>

--- src/engine/Collection.java
+++ src/engine/Collection.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.ListIterator;
 
 abstract class Collection<E> implements List<E> {
-	private List<E> elements = new ArrayList<E>();
+	List<E> elements = new ArrayList<E>();
 
 	abstract void render(Graphics graphics);
 
