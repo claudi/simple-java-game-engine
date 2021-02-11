aa38a3ad70f84be6d916d188334678c8633d59a3
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-02-11 13:44:46 +0100

    Implement sprite and constructor in Jugador

--- src/spaceInvaders/Jugador.java
+++ src/spaceInvaders/Jugador.java
@@ -1,11 +1,19 @@
 package spaceInvaders;
 
+import java.awt.Color;
+
+import engine.Game;
 import engine.Player;
 
 public class Jugador extends Player {
 	private boolean vb_l = false, vb_r = false;
 	private int v_l = 0, v_r = 0;
 	private int v_increment = 10;
+	static Color[][] pixel_array = Game.sprites("player_sprite");
+	
+	public Jugador(int pos_x, int pos_y) {
+		super(pos_x, pos_y);
+	}
 	
 	protected void moveLeftCommand(boolean status) {
 		if(status && !vb_l) {
