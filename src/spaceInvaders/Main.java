85d08ed0dc862114dc00d30135fa4246ea3f0424
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-02-13 14:26:36 +0100

    Improve background generation

--- src/spaceInvaders/Main.java
+++ src/spaceInvaders/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Game game = new Game("Space Invaders");
-		game.setBackground(generateBackground(game.getWidth(), game.getHeight()));
+		game.setBackground(generateBackground(game));
 
 		Jugador jugador = new Jugador(game.getWidth() / 2, Math.round(0.9f * game.getHeight()));
 		jugador.setBounds(game);
@@ -27,7 +27,9 @@ public class Main {
 		game.quitGame();
 	}
 
-	private static BufferedImage generateBackground(int width, int height) {
+	private static BufferedImage generateBackground(Game game) {
+		int width = game.getWidth();
+		int height = game.getHeight();
 		BufferedImage background = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
 		for (int i = 0; i < width; i++) {
 			for (int j = 0; j < height; j++) {
